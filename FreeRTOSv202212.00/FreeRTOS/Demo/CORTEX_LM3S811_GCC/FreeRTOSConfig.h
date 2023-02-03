359c324c983728db8f6be6a44de4788f61aa1625
akmsw <franbonino82@gmail.com>
2023-02-03 20:26:52 -0300

    rollback

--- FreeRTOSv202212.00/FreeRTOS/Demo/CORTEX_LM3S811_GCC/FreeRTOSConfig.h
+++ FreeRTOSv202212.00/FreeRTOS/Demo/CORTEX_LM3S811_GCC/FreeRTOSConfig.h
@@ -44,15 +44,13 @@
 #define configUSE_TICK_HOOK			0
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 20000000 )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 300 )
+#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 150 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 7000 ) )
 #define configMAX_TASK_NAME_LEN		( 10 )
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		0
-#define configSUPPORT_DYNAMIC_ALLOCATION            1
 #define configMAX_PRIORITIES		( 5 )
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
