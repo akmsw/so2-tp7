fc167cc7a42de615311392759bbaa273c1e0bb87
akmsw <franbonino82@gmail.com>
2023-02-03 20:12:58 -0300

    prueba

--- FreeRTOSv202212.00/FreeRTOS/Demo/CORTEX_LM3S811_GCC/FreeRTOSConfig.h
+++ FreeRTOSv202212.00/FreeRTOS/Demo/CORTEX_LM3S811_GCC/FreeRTOSConfig.h
@@ -50,8 +50,9 @@
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		0
-
+#define configSUPPORT_DYNAMIC_ALLOCATION            1
 #define configMAX_PRIORITIES		( 5 )
+#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
